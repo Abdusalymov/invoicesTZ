5e0ccb9731fe0fead64feda2b73a1db53b03fad3
Ildar <irbis180288@yandex.ru>
2019-03-17 21:23:47 +0500

    delte node_modules

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
-app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+// app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.listen(port, () => {
   console.log('listening on %d', port);
 });
