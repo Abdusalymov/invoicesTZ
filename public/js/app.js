da9647ccbc415cac44e1d178a5492dc310605704
Ildar <irbis180288@yandex.ru>
2019-03-19 13:08:59 +0500

    invoice adding complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 $(function() {
     const el = $('#app');
     const mainTemplate = Handlebars.compile($('#main-template').html());
-    // const ratesTemplate = Handlebars.compile($('#rates-template').html());
     const createInvoiceTemplate = Handlebars.compile($('#create-invoice-template').html());
 
     var router = new Router({
@@ -29,46 +28,16 @@ $(function() {
                 let html = createInvoiceTemplate();
                 el.html(html);
             })
-            .add('search', function () {
-                console.log('Search Page');
-                document.getElementById('app').innerHTML = 'Search for: ' + this.query.q;
-            }, {
-                unloadCb: function (async) {
-                    if (async){
-                        console.warn("You have unsaved data!");
-                        return confirm("You have unsaved data! Continue?");
-                    }
-                    return false;
-                }
-            })
-            .add('hello/(:any)', function (name) {
-                console.log('Hello, ' + name, this.state);
-                document.getElementById('app').innerHTML = 'Hello, ' + name;
-            })
-            .add('about', function () {
-                console.log('About Page!!!');
-                document.getElementById('app').innerHTML = 'About Page';
-            })
-            .remove('about')
             .check()
             .addUriListener()
-            // .navigateTo('hello/World', {foo: "bar"})
             .refresh();
 
             $('a').on('click', function(event){
                 // Block browser page load
                 event.preventDefault();
-                console.log('event work!!!')
-                // Highlight Active Menu on Click
                 const target = $(event.target);
-                // $('.item').removeClass('active');
-                // target.addClass('active');
-                
                 // Navigate to clicked url
                 const href = target.attr('href');
-                console.log(href);
-                // const path = href.substr(href.lastIndexOf('/'));
-                // console.log(path);
                 router.navigateTo(href);
             });
     window.router = router;
